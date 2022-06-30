215aed950ab1cbf420df701a336ff6bb5696155c
ccjake <jake.cc132@gmail.com>
2022-06-30 18:49:51 +0200

    implement selection function

--- Web/app.py
+++ Web/app.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for,flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 from os.path import join, dirname, realpath
 from celonis_connect import Celonis_Connect
-
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 # enable debugging mode
 app.config["DEBUG"] = True
 
 # Uploads folder
-UPLOAD_FOLDER = "static/Uploads"
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/Uploads/')
 ALLOWED_EXTENSIONS = set(["xes"])
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 cn = "no connection"
+result = None
+# pools = None
+# datamodels = None
+# tables = None
+
+pools = ["1"]
+datamodels = {"1":["aa"]}
+tables = {"aa":["bb"]}
 # cn = Celonis_Connect()
+
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1] in ALLOWED_EXTENSIONS
@@ -22,7 +31,7 @@ def allowed_file(filename):
 
 @app.route("/", methods=["POST", "GET"])
 def login_celonis():
-    result = None
+    global result
     global cn
     if request.method == "POST":
         celonis_url = str(request.form.get("celonis_url"))
@@ -31,12 +40,26 @@ def login_celonis():
         try:
             cn = Celonis_Connect(celonis_url=celonis_url, api_token=celonis_toke)
             result = "Connect uccessfully!"
-            return render_template("celonis_login.html",result = result)
+            global pools
+            global datamodels
+            global tables
+            pools = [pool.name for pool in cn.get_pools()]
+            datamodels = {
+                pool.name: [datamodel.name for datamodel in pool.datamodels]
+                for pool in cn.get_pools()
+            }
+            tables = {
+                datamodel.name: [table.name for table in datamodel.tables]
+                for datamodel in cn.get_datamodels()
+            }
+
+            ## return pools, datamodels, tables for select
+            return redirect("/select_function")
         except:
             cn = "no connection"
             result = "Connect Falied!"
 
-    return render_template("celonis_login.html",result=result)
+    return render_template("celonis_login.html", result=result)
 
 
 @app.route("/select_function", methods=["POST", "GET"])
@@ -44,33 +67,95 @@ def select():
     return render_template("select_func.html")
 
 
-# manage the pools, datamodels and tables
+# manage the pools, datamodels and tables;
 @app.route("/table_manager", methods=["GET", "POST"])
 def table_management():
     # get the uploaded file
-    global cn
-    pools = [pool.name for pool in cn.get_pools()]
-    datamodels = {pool.name:[datamodel.name for datamodel in pool.datamodels] for pool in cn.get_pools()}
-    tables = {datamodel.name:[table.name for table in datamodel.tables] for datamodel in cn.get_datamodels()}
-    print(datamodels)
+    error = None
+    if request.method == "POST":
+        global cn
+        global pools
+        global datamodels
+        global tables
+        if "add_pool" in request.form:
+            try:
+                pool_name = str(request.form.get("add_pool"))
+                print("request.values",request.values)
+                print("request.form",request.form)
+
+                print(pool_name)
+                cn.c.create_pool(pool_name)
+                pools = [pool.name for pool in cn.get_pools()]
+                return render_template(
+                    "table_management.html",
+                    pools=pools,
+                    datamodels=datamodels,
+                    tables=tables,
+                )
+            except:
+                return render_template(
+                    "table_management.html",
+                    pools=pools,
+                    datamodels=datamodels,
+                    tables=tables,
+                    error="pool already exists",
+                )
+
+        if "add_datamodel" in request.form:
+            try:
+                pool_name = str(request.form.get("selected_pool"))
+                datamodel_name = str(request.form.get("add_datamodel"))
+                cn.c.create_datamodel(datamodel_name,pool_name)
+                datamodels = {
+                    pool.name: [datamodel.name for datamodel in pool.datamodels]
+                    for pool in cn.get_pools()
+                }
+                return render_template(
+                    "table_management.html",
+                    pools=pools,
+                    datamodels=datamodels,
+                    tables=tables,
+                )
+            except:
+                return render_template(
+                    "table_management.html",
+                    pools=pools,
+                    datamodels=datamodels,
+                    tables=tables,
+                    error="datamodel already exists",
+                )
 
 
+        if "add_table" in request.files:
+            # try:
+            pool_name = str(request.form.get("selected_pool"))
+            datamodel_name = str(request.form.get("selected_datamodel"))
+            table = request.files['add_table']
+            if table :
+                tablename = secure_filename(table.filename)
+                file_path = os.path.join(app.config["UPLOAD_FOLDER"], tablename)
+                # set the file path
+                table.save(file_path)
 
-    # if request.method == "POST":  # 当以post方式提交数据时
-    #     pool_name = request.form.get('pool_name')
-    #     print(pool_name)
-    #     return redirect("/upload")
-    #     uploaded_file = request.files["event_log"]
-    #     if uploaded_file.filename != "":
-    #
-    #         file_path = os.path.join(
-    #             app.config["UPLOAD_FOLDER"], uploaded_file.filename
-    #         )
-    #         print(uploaded_file.filename)
-    #
-    #     uploaded_file.save(file_path)
-    # save the file
-    return render_template("table_management.html",pools = pools,datamodels = datamodels,tables = tables)
+            #     return render_template(
+            #         "table_management.html",
+            #         pools=pools,
+            #         datamodels=datamodels,
+            #         tables=tables,
+            #     )
+            # except:
+            #     print("except")
+            #     return render_template(
+            #         "table_management.html",
+            #         pools=pools,
+            #         datamodels=datamodels,
+            #         tables=tables,
+            #         error="table already exists",
+            #     )
+
+    return render_template(
+        "table_management.html", pools=pools, datamodels=datamodels, tables=tables
+    )
 
 
 if __name__ == "__main__":
