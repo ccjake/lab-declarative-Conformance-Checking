746214327aafdd353903ae1aad21ff1b8812f6c8
ccjake <jake.cc132@gmail.com>
2022-07-01 05:27:24 +0200

    upload file finished

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import os
 from os.path import join, dirname, realpath
 from celonis_connect import Celonis_Connect
 from werkzeug.utils import secure_filename
-import sys
-# sys.path.append(r'./')
 
+from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py import convert_to_dataframe
 
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 # Uploads folder
-UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/Uploads/')
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), "static/Uploads/")
 ALLOWED_EXTENSIONS = set(["xes"])
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
@@ -25,8 +25,8 @@ result = None
 # tables = None
 
 pools = ["1"]
-datamodels = {"1":["aa"]}
-tables = {"aa":["bb"]}
+datamodels = {"1": ["aa"]}
+tables = {"aa": ["bb"]}
 # cn = Celonis_Connect()
 
 
@@ -82,11 +82,13 @@ def table_management():
         global pools
         global datamodels
         global tables
+
+        ## add pool
         if "add_pool" in request.form:
             try:
                 pool_name = str(request.form.get("add_pool"))
-                print("request.values",request.values)
-                print("request.form",request.form)
+                print("request.values", request.values)
+                print("request.form", request.form)
 
                 print(pool_name)
                 cn.c.create_pool(pool_name)
@@ -106,11 +108,13 @@ def table_management():
                     error="pool already exists",
                 )
 
+
+        ## add datamodel
         if "add_datamodel" in request.form:
             try:
                 pool_name = str(request.form.get("selected_pool"))
                 datamodel_name = str(request.form.get("add_datamodel"))
-                cn.c.create_datamodel(datamodel_name,pool_name)
+                cn.c.create_datamodel(datamodel_name, pool_name)
                 datamodels = {
                     pool.name: [datamodel.name for datamodel in pool.datamodels]
                     for pool in cn.get_pools()
@@ -131,32 +135,65 @@ def table_management():
                 )
 
 
+        ## add table
         if "add_table" in request.files:
-            # try:
-            pool_name = str(request.form.get("selected_pool"))
-            datamodel_name = str(request.form.get("selected_datamodel"))
-            table = request.files['add_table']
-            if table :
-                tablename = secure_filename(table.filename)
-                file_path = os.path.join(app.config["UPLOAD_FOLDER"], tablename)
-                # set the file path
-                table.save(file_path)
-
-            #     return render_template(
-            #         "table_management.html",
-            #         pools=pools,
-            #         datamodels=datamodels,
-            #         tables=tables,
-            #     )
-            # except:
-            #     print("except")
-            #     return render_template(
-            #         "table_management.html",
-            #         pools=pools,
-            #         datamodels=datamodels,
-            #         tables=tables,
-            #         error="table already exists",
-            #     )
+            try:
+                pool_name = str(request.form.get("selected_pool"))
+                datamodel_name = str(request.form.get("selected_datamodel"))
+                table = request.files["add_table"]
+                if table:
+                    tablename = secure_filename(table.filename)
+                    file_path = os.path.join(app.config["UPLOAD_FOLDER"], tablename)
+                    # set the file path
+                    table.save(file_path)
+                    log_path = join(dirname(realpath(__file__)), file_path)
+                    log = xes_importer.apply(log_path)
+                    df_act = convert_to_dataframe(log)
+                    df_act.rename(
+                        columns={"case:concept:name": "CASE ID"}, inplace=True
+                    )
+                    colnames = df_act.columns
+                    pool = cn.get_pools().find(pool_name)
+                    datamodel = cn.get_datamodels().find(datamodel_name)
+                    pool.create_table(df_act, tablename)
+                    tablename = tablename.replace(".", "_")
+                    datamodel.add_table_from_pool(table_name=tablename)
+                    for colname in colnames:
+                        if "CASE" in colname:
+                            case_col = colname
+                            continue
+                        if "time" in colname:
+                            time_col = colname
+                            continue
+                        if "concept:name" in colname:
+                            act_col = colname
+                            continue
+                    datamodel.create_process_configuration(
+                        activity_table=tablename,
+                        case_column=case_col,
+                        activity_column=act_col,
+                        timestamp_column=time_col,
+                    )
+                    datamodel.reload()
+                    tables = {
+                        datamodel.name: [table.name for table in datamodel.tables]
+                        for datamodel in cn.get_datamodels()
+                    }
+                return render_template(
+                    "table_management.html",
+                    pools=pools,
+                    datamodels=datamodels,
+                    tables=tables,
+                )
+            except:
+                print("except")
+                return render_template(
+                    "table_management.html",
+                    pools=pools,
+                    datamodels=datamodels,
+                    tables=tables,
+                    error="table already exists",
+                )
 
     return render_template(
         "table_management.html", pools=pools, datamodels=datamodels, tables=tables
